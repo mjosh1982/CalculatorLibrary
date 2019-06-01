0218626d5e23c1c94c2025945d297d379da3e7f9
Manoj Joshi <manojoshi1982@gmail.com>
2019-06-01 21:24:15 +0530

    Adding test file for calculator.py

---
+++ calculator/test_calculator.py
@@ -0,0 +1,10 @@
+import calculator
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_subtract(self):
+        assert 5 == calculator.subtract(10, 5)
